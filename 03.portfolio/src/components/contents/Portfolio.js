b446e4d36e1a660c801474473a708cd71dd6ba52
agungTuanany <agung.tuanany@gmail.com>
2018-08-01 23:55:15 +0700

    React - Portfolio --Add db.json

--- 03.portfolio/src/components/contents/Portfolio.js
+++ 03.portfolio/src/components/contents/Portfolio.js
@@ -2,120 +2,37 @@ import React, { Component } from 'react';
 import { Row, Col, ButtonToolbar, Button } from 'react-bootstrap';
 
 class Portfolio extends Component {
-  /*    filterSelection("all")
-    const filterSelection = (c) => {
-        var x, i;
-        x = document.getElementByClassName("Col");
-        if (c === "all") c = "";
-
-        for(i = 0; i < x.length; i++) {
-
-        }
-    }
-    */
   constructor(props) {
     super(props);
-    this.filterSelection = this.filterSelection.bind(this);
-  }
 
-  filterSelection(c) {
-    //console.log("button was clicked");
-    var i, x;
-    x = document.getElementsByClassName("column");
-    if (c === "all") c = "";
-    for (i = 0; i < x.length; i++) {
-      removeClass(x[i], "show");
-      if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+    this.state = {
+      data : "Initial data...."
     }
+    this.filterSelection = this.filterSelection.bind(this);
+  };
+
+  filterSelection() {
+    this.setState({data: "data updated.."})
   }
 
   render() {
     return (
       <div>
+        <h1>Portfolio</h1>
         <Row>
           <Col md={12}>
-            <div className="content-inner text-center">
-              <h4>Portfolio-Component</h4>
-              <ButtonToolbar>
-                <Button
-                  className="btn active"
-                  onClick={this.filterSelection}
-                  bsStyle="link">
-                  All
-                </Button>
-                <Button
-                  className="btn"
-                  onClick={this.filterSelection}
-                  bsStyle="link">
-                  Web
-                </Button>
-              </ButtonToolbar>
-            </div>
+            <Button
+              className="btn-active"
+              onClick={this.filterSelection}
+            >
+              All
+            </Button>
           </Col>
         </Row>
-        <Row>
-          <Col md={12} className="web column">
-            <div className="content">
-              <img
-                src="portfolio-1.jpg"
-                alt="potfolio-1.jpg"
-                style={{ width: ' 100%' }}
-              />
-              <h4>Portfolio</h4>
-              <p>Lorem ipsum dolo</p>
-            </div>
-          </Col>
-          <Col md={12} className="web column">
-            <div className="content">
-              <img
-                src="portfolio-1.jpg"
-                alt="potfolio-1.jpg"
-                style={{ width: ' 100%' }}
-              />
-              <h4>Portfolio</h4>
-              <p>Lorem ipsum dolo</p>
-            </div>
-          </Col>
-          <Col md={12} className="web column">
-            <div className="content">
-              <img
-                src="portfolio-1.jpg"
-                alt="potfolio-1.jpg"
-                style={{ width: ' 100%' }}
-              />
-              <h4>Portfolio</h4>
-              <p>Lorem ipsum dolo</p>
-            </div>
-          </Col>
-        </Row>
+            <h4>{this.state.data}</h4>
       </div>
-    );
+    )
   }
 }
-
-function addClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) === -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
-
-function removeClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
-
-var btnContainer = document.getElementById("myBtnContainer");
 
 export default Portfolio;
